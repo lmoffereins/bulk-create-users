fb2bde45788e43f6b529c0283e10a3f3dc6106cf
Laurens Offereins <lmoffereins@gmail.com>
2015-02-14 17:59:14 +0100

    Update to version 1.1.0

--- bulk-create-users.php
+++ bulk-create-users.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Bulk Create Users
  * Description:       Create/import/update multiple users at once 
  * Plugin URI:        https://github.com/lmoffereins/bulk-create-users/
- * Version:           1.0.1
+ * Version:           1.1.0
  * Author:            Laurens Offereins
  * Author URI:        https://github.com/lmoffereins/
  * Network:           true
@@ -72,7 +72,7 @@ final class Bulk_Create_Users {
 
 		/** Versions **********************************************************/
 
-		$this->version      = '1.0.1';
+		$this->version      = '1.1.0';
 
 		/** Paths *************************************************************/
 
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Offereins
 Tags: bulk, create, import, upload, users, csv, buddypress, xprofile, groups, multisite
 Requires at least: 4.0
 Tested up to: 4.1
-Stable tag: 1.0.1
+Stable tag: 1.1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
